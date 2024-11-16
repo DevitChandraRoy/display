42907450dae10fe7a82ef5a3bab46df990682e6d
Devit Roy <devitroy15@gmail.com>
2024-11-16 12:18:28 -0800

    2nd Version

--- src/display/Display.java
+++ src/display/Display.java
@@ -7,6 +7,12 @@ public class Display {
     public static void main(String[] args)
     {
         System.out.println("text1");
+        System.out.println("text1");
+        System.out.println("text1");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Bangladesh");
+        }
         
     }
     
